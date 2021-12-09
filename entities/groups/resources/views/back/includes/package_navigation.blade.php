8fcc9ab4c0dcd9580787e007e9e124f629f49788
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-09 17:22:13 +0300

    Fix navigation

--- entities/groups/resources/views/back/includes/package_navigation.blade.php
+++ entities/groups/resources/views/back/includes/package_navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.banners-package.groups.*') }}">
+<li class="{{ isActiveRoute('back.banners-package.groups.*', 'mm-active') }}">
     <a href="{{ route('back.banners-package.groups.index') }}">Группы</a>
 </li>
--- entities/places/resources/views/back/includes/package_navigation.blade.php
+++ entities/places/resources/views/back/includes/package_navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.banners-package.places.*') }}">
+<li class="{{ isActiveRoute('back.banners-package.places.*', 'mm-active') }}">
     <a href="{{ route('back.banners-package.places.index') }}">Расположения</a>
 </li>
