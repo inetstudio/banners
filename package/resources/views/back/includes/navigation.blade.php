f6e81b51c3cfcd7c0bc3ff262c7344ab12cec3ea
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-09 22:00:52 +0300

    Fix menu icon

--- package/resources/views/back/includes/navigation.blade.php
+++ package/resources/views/back/includes/navigation.blade.php
@@ -1,6 +1,6 @@
 <li class="{{ isActiveRoute('back.banners-package.*', 'mm-active') }}">
     <a href="#" aria-expanded="false">
-        <i class="fa fa-ad"></i> <span class="nav-label">Баннеры</span><span class="fa arrow"></span>
+        <i class="fab fa-adversal"></i> <span class="nav-label">Баннеры</span><span class="fa arrow"></span>
     </a>
     <ul class="nav nav-second-level">
         @include('admin.module.banners-package.banners::back.includes.package_navigation')
